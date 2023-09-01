70f2aa1a2da5c32db5ae85a3d7adbc0ed7067b39
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2023-09-02 01:03:31 +0600

    done: sorting scores and displays them, sound work (it would be weird to make a game without sound effects)

--- Maze Game/Player.cpp
+++ Maze Game/Player.cpp
@@ -3,13 +3,18 @@
 
 #include <Windows.h>
 #include <iostream>
+#include <sstream>
 #include <conio.h>
 #include <chrono>
 #include <fstream>
 #include <thread>
+#include <vector>
+#include <algorithm>
 
 
 #define OFFSET 5
+
+// keys
 #define ARROW_KEY 224
 #define ARROW_UP 72
 #define ARROW_RIGHT 77
@@ -17,6 +22,13 @@
 #define ARROW_LEFT 75
 #define ENTER_KEY 13
 #define BACKSPACE_KEY 8
+
+// sounds
+#define SELECT_SOUND 262
+#define ENTER_SOUND 988
+#define MOVE_SOUND 500
+#define EXIT_SOUND 300
+#define SOLVED_SOUND 523
 
 void clearScreen() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -76,10 +88,12 @@ void Player::menu() {
 
             if (key == ARROW_UP && pos != 0) {
                 pos--;
+                Beep(SELECT_SOUND, 20);
                 clearScreen();
             }
             else if (key == ARROW_DOWN && pos != 3) {
                 pos++;
+                Beep(SELECT_SOUND, 20);
                 clearScreen();
             }
         }
@@ -87,12 +101,17 @@ void Player::menu() {
             switch (pos)
             {
             case 0: // play
+                Beep(784, 150);
+                Beep(988, 150);
+                Beep(1319, 300);
                 this->play();
                 break;
             case 1: // scoreboard
+                Beep(ENTER_SOUND, 150);
                 this->scoreboard();
                 break;
             case 2: // about
+                Beep(ENTER_SOUND, 150);
                 this->about();
                 break;
             case 3: // exit
@@ -100,6 +119,7 @@ void Player::menu() {
             }
 
             SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)0, (short)OFFSET + 8 });
+            Beep(EXIT_SOUND, 300);
             std::cout << "Closing...";
             return;
         }
@@ -108,8 +128,9 @@ void Player::menu() {
 }
 
 void Player::play() {
-    std::string time = "00:00:00";
     clearScreen();
+    std::string time = "00:00:00";
+    int64_t minutes, seconds, milliseconds;
 
     Maze* maze = new Maze(this->level + 5);
     maze->setup();
@@ -128,15 +149,17 @@ void Player::play() {
 
             // duration calc
             auto endTime = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime);
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 
-            std::string hr = std::to_string(duration.count() / 3600);
-            std::string min = std::to_string((duration.count() % 3600) / 60);
-            std::string sec = std::to_string(duration.count() % 60);
+            minutes = std::chrono::duration_cast<std::chrono::minutes>(duration).count();
+            duration -= std::chrono::minutes(minutes);
+            seconds = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+            duration -= std::chrono::seconds(seconds);
+            milliseconds = duration.count();
 
-            if (hr.length() == 1) {
-                hr = "0" + hr;
-            }
+            std::string min = std::to_string(minutes);
+            std::string sec = std::to_string(seconds);
+            std::string milli = std::to_string(milliseconds);
 
             if (min.length() == 1) {
                 min = "0" + min;
@@ -146,26 +169,30 @@ void Player::play() {
                 sec = "0" + sec;
             }
 
-            time = hr + ":" + min + ":" + sec;
+            time = min + ":" + sec + ":" + milli;
 
             // player move
             if (key == ARROW_UP && !maze->isWall(this->row, this->col, 0)) {
                 this->row--;
+                Beep(MOVE_SOUND, 10);
                 clearScreen();
                 maze->draw(this->row, this->col, this->name, this->level, time);
             }
             else if (key == ARROW_RIGHT && !maze->isWall(this->row, this->col, 1)) {
                 this->col++;
+                Beep(MOVE_SOUND, 10);
                 clearScreen();
                 maze->draw(this->row, this->col, this->name, this->level, time);
             }
             else if (key == ARROW_DOWN && !maze->isWall(this->row, this->col, 2)) {
                 this->row++;
+                Beep(MOVE_SOUND, 10);
                 clearScreen();
                 maze->draw(this->row, this->col, this->name, this->level, time);
             }
             else if (key == ARROW_LEFT && !maze->isWall(this->row, this->col, 3)) {
                 this->col--;
+                Beep(MOVE_SOUND, 10);
                 clearScreen();
                 maze->draw(this->row, this->col, this->name, this->level, time);
             }
@@ -183,13 +210,14 @@ void Player::play() {
                 return;
             }
 
-            file << this->name << "," << this->level << "," << time << "\n";
+            file << this->name << "," << this->level << "," << minutes  << "," << seconds << "," << milliseconds << "\n";
 
             file.close();
 
             // show
             clearScreen();
             maze->draw(this->row, this->col, this->name, this->level, time, this->solved);
+            Beep(523, 300);
             std::this_thread::sleep_for(std::chrono::seconds(1));
 
             // increase level
@@ -213,6 +241,7 @@ void Player::play() {
 
         // <- back
         if (key == BACKSPACE_KEY) {
+            Beep(EXIT_SOUND, 300);
             break;
         }
 
@@ -222,15 +251,33 @@ void Player::play() {
     this->menu();
 }
 
+bool sort_scores(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    int aLevel = std::stoi(a[1]);
+    int bLevel = std::stoi(b[1]);
+
+    if (aLevel > bLevel) {
+        return true;
+    }
+    else if (aLevel < bLevel) {
+        return false;
+    }
+
+    for (int i = 2; i <= 4; ++i) {
+        int aValue = std::stoi(a[i]);
+        int bValue = std::stoi(b[i]);
+
+        if (aValue < bValue) {
+            return true;
+        }
+        else if (aValue > bValue) {
+            return false;
+        }
+    }
+
+    return false;
+}
+
 void Player::scoreboard() {
-    clearScreen();
-
-    // header
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET });
-    std::cout << "SCOREBOARD";
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET + 1 });
-    std::cout << "--------------------";
-
     // scores
     std::ifstream file("scores.txt");
 
@@ -240,17 +287,70 @@ void Player::scoreboard() {
     }
 
     std::string line;
+    std::vector<std::vector<std::string>> scores;
+
     while (std::getline(file, line)) {
-        std::cout << line << "\n";
+        std::istringstream stream(line);
+        std::string token;
+        std::vector<std::string> tokens;
+        char delimiter = ',';
+
+        while (std::getline(stream, token, delimiter)) {
+            tokens.push_back(token);
+        }
+
+        scores.push_back(tokens);
+    }
+    
+    file.close();
+    std::sort(scores.begin(), scores.end(), sort_scores);
+
+    // print
+    int key;
+    int start = 0;
+
+    while (true) {
+        clearScreen();
+        // header
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET });
+        std::cout << "SCOREBOARD";
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET + 1 });
+        std::cout << "--------------------";
+
+        // scores
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)(OFFSET + 3) });
+        std::cout << "\tNAME\tLEVEL\tDURATION";
+
+        size_t size = scores.size() > (5 + start) ? (5 + start) : scores.size();
+
+        for (size_t i = start; i < size; i++) {
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)(OFFSET + 4 + i - start) });
+            std::cout << (i + 1) << "\t" << scores[i][0] << "\t" << scores[i][1] << "\t" << scores[i][2] << ":" << scores[i][3] << ":" << scores[i][4];
+        }
+
+        // <- back
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET + 11 });
+        std::cout << "<- Press any key to go to main menu ....";
+
+        key = _getch();
+        if (key == ARROW_KEY) {
+            key = _getch();
+
+            if (key == ARROW_UP && start != 0) {
+                Beep(500, 10);
+                start--;
+            }
+            else if (key == ARROW_DOWN && start != scores.size() - 5) {
+                Beep(500, 10);
+                start++;
+            }
+        }
+        else {
+            break;
+        }
     }
 
-    file.close();
-
-    // <- back
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET + 11 });
-    std::cout << "<- Press any key to go to main menu ....";
-
-    _getch();
+    Beep(EXIT_SOUND, 300);
     this->menu();
 }
 
@@ -285,6 +385,7 @@ void Player::about() {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ (short)OFFSET, (short)OFFSET + 11 });
     std::cout << "<- Press any key to go to main menu ....";
 
-    _getch();
+    int key = _getch();
+    Beep(EXIT_SOUND, 300);
     this->menu();
 }
